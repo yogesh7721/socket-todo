f4bc0897856ae971867751445ddd47820cca8bee
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-26 15:03:18 +0530

    initial setup

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -1,16 +1,21 @@
 // CRUD
 
 const asyncHandler = require('express-async-handler')
+const { default: Todo } = require('../../client/src/pages/Todo')
 
 exports.createTodo = asyncHandler(async (req, res) => {
+    await Todo.create(req.body)
     res.json({ message: "createTodo success" })
 })
 exports.readTodo = asyncHandler(async (req, res) => {
-    res.json({ message: "readTodo success" })
+    const result = await Todo.find()
+    res.json({ message: "readTodo success", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndUpdate(req.params.id, req.body)
     res.json({ message: "updateTodo success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
+    await Todo.findByIdAndDelete(req.params.id)
     res.json({ message: "deleteTodo success" })
 })
