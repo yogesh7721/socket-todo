da9b12abd27395fd923d516f148109a8a84c998a
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-28 14:08:20 +0530

    404 fixe

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors")
 const mongoose = require("mongoose")
 const { app, httpServer } = require("./socket/socket")
 require("dotenv").config()
+const path = require("path")
 
 // const app = express()
 
@@ -10,11 +11,15 @@ require("dotenv").config()
 app.use(express.json())
 app.use(express.static("dist"))
 app.use(cors({ origin: true, credentials: true }))  //credential for cookie
+
 // step 2 routes
 app.use("/api/notes", require("./routes/todo.routes"))
+
 // step 3   404 route
 app.use("*", (req, res) => {
-    res.status(404).json({ message: "Resource Not Found 404" })
+    //                   Absolute Path
+    res.sendFile(path.join(__dirname, "dist", "index.html"))
+    //  res.status(404).json({ message: "Resource Not Found 404" })
 })
 // step 5 error handler
 app.use((err, req, res, next) => {
