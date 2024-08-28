a451da7d93de82395714e0b49cae82d3bd49176a
yogesh shelke <shelkeyogesh874@gmail.com>
2024-08-28 13:01:38 +0530

    change servers

--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -15,7 +15,7 @@ exports.readTodo = asyncHandler(async (req, res) => {
     res.json({ message: "readTodo success", result })
 })
 exports.updateTodo = asyncHandler(async (req, res) => {
-    await Todo.findByIdAndUpdate(req.params.id, req.body)
+    await Todo.findByIdAndUpdate(req.params.id, { ...req.body, complete: true })
     res.json({ message: "updateTodo success" })
 })
 exports.deleteTodo = asyncHandler(async (req, res) => {
